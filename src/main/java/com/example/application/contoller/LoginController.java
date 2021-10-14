3d6fe072b046ef3877ec53e1846f448f14593fd5
nemoskii <sk.nmt8@gmail.com>
2021-10-14 10:30:19 +0900

    test

--- src/main/java/com/example/application/contoller/LoginController.java
+++ src/main/java/com/example/application/contoller/LoginController.java
@@ -1,5 +1,13 @@
 package com.example.application.contoller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
 
+	    @GetMapping
+		public String getLogin() {
+			return "login/login";
+		}
 }
