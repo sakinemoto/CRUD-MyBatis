05fbaf02496164dd2a15ffc0a358081239cf2363
nemoskii <sk.nmt8@gmail.com>
2021-10-14 10:30:33 +0900

    test

--- src/main/java/com/example/application/contoller/LoginController.java
+++ src/main/java/com/example/application/contoller/LoginController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	    @GetMapping
-		public String getLogin() {
-			return "login/login";
-		}
+        @GetMapping
+        public String getLogin() {
+            return "login/login";
+        }
 }
