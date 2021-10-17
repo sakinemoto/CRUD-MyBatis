3716b613ddd2a0a05040a5862f09de9b06de8fad
nemoskii <sk.nmt8@gmail.com>
2021-10-18 07:03:19 +0900

    多言語化

--- src/main/java/com/example/contoller/SignupController.java
+++ src/main/java/com/example/contoller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.contoller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,11 @@ public class SignupController {
     @Autowired
     private UserApplicationService userApplicationService;
     
-    
+    /*ユーザー登録画面を表示*/
     @GetMapping("/signup")
-    public String getSignup(Model model) {
+    public String getSignup(Model model,Locale locale) {
         //性別を取得
-        Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
         
         //ユーザー登録画面に遷移
