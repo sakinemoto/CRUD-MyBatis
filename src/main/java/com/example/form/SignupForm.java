703ff6e9acc285a1980a5cf9cbc8dceaab9c519d
nemoskii <sk.nmt8@gmail.com>
2021-10-21 16:57:19 +0900

    バリデーション

--- src/main/java/com/example/form/SignupForm.java
+++ src/main/java/com/example/form/SignupForm.java
@@ -16,27 +16,27 @@ import lombok.Data;
 
 @Data
 public class SignupForm {
-    
+
     @NotBlank(groups = ValidGroup1.class)
     @Email(groups = ValidGroup2.class)
     private String userId;
-    
+
     @NotBlank(groups = ValidGroup1.class)
-    @Length(min = 4,max = 100, groups = ValidGroup2.class)
+    @Length(min = 4, max = 100, groups = ValidGroup2.class)
     @Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
     private String password;
-    
+
     @NotBlank(groups = ValidGroup1.class)
     private String userName;
-    
+
     @DateTimeFormat(pattern = "yyyy/MM/dd")
-	@NotNull(groups = ValidGroup1.class)
-	private Date birthday;
-    
-    @Min(value=20, groups = ValidGroup2.class)
-    @Max(value = 100,groups = ValidGroup2.class)
+    @NotNull(groups = ValidGroup1.class)
+    private Date birthday;
+
+    @Min(value = 20, groups = ValidGroup2.class)
+    @Max(value = 100, groups = ValidGroup2.class)
     private Integer age;
-    
+
     @NotNull(groups = ValidGroup1.class)
     private Integer gender;
 }
