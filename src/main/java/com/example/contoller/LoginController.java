b89deec5941f22830f503dff930ec95ca2b3dae0
nemoskii <sk.nmt8@gmail.com>
2021-10-28 00:10:05 +0900

    画面作成

--- src/main/java/com/example/contoller/LoginController.java
+++ src/main/java/com/example/contoller/LoginController.java
@@ -2,13 +2,20 @@ package com.example.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-    //*ログイン画面を表示
+    /**ログイン画面を表示*/
     @GetMapping("/login")
     public String getLogin() {
         return "login/login";
+    }
+    
+    /**ユーザー一覧画面にリダイレクト*/
+    @PostMapping("/login")
+    public String postLogin() {
+        return "redirect:/user/list";
     }
 }
