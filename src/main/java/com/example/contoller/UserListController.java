cf9f3fba292fa0437b0836c6904ffd1cb0a9a627
nemoskii <sk.nmt8@gmail.com>
2021-11-02 01:17:12 +0900

    select複数件

--- src/main/java/com/example/contoller/UserListController.java
+++ src/main/java/com/example/contoller/UserListController.java
@@ -1,18 +1,35 @@
 package com.example.contoller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
 
-    //**ユーザー一覧画面を表示*/
+    @Autowired
+    private UserService userService;
+
+    // **ユーザー一覧画面を表示*/
     @GetMapping("/list")
-    public String getUserList() {
-        //ユーザー一覧画面を表示
+    public String getUserList(Model model) {
+
+        // ユーザー一覧取得
+        List<MUser> userList = userService.getUsers();
+
+        // *Modelに登録
+        model.addAttribute("userList", userList);
+
+        // ユーザー一覧画面を表示
         return "user/list";
     }
-    
+
 }
